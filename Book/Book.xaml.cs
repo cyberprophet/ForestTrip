5d9559049ebfce2a653a892ba86c280910c25c78
sangwoo park <cyberprophet@hanmail.net>
2024-10-12 22:07:04 +0900

    collect strings marked up

--- Book/Book.xaml.cs
+++ Book/Book.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 
 namespace ShareInvest;
 
@@ -7,5 +8,18 @@ public partial class Book : Window
     public Book()
     {
         InitializeComponent();
+
+        webView = new CoreWebView(webView2);
+
+        _ = webView.OnInitializedAsync("https://www.foresttrip.go.kr/main.do");
     }
+
+    void OnStateChanged(object sender, EventArgs _)
+    {
+#if DEBUG
+        Debug.WriteLine(sender);
+#endif  
+    }
+
+    readonly CoreWebView webView;
 }
